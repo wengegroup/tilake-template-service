760ca7787331aab7f11a43e3e4989146b8a02840
Lenovo <1002364430@qq.com>
2021-10-26 16:19:11 +0800

    优化vo类

--- src/main/java/com/wenge/tilake/vo/TableBasicInfoVo.java
+++ src/main/java/com/wenge/tilake/vo/TableBasicInfoVo.java
@@ -1,5 +1,6 @@
 package com.wenge.tilake.vo;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -9,11 +10,23 @@ import java.util.Date;
 @Accessors(chain = true)
 public class TableBasicInfoVo {
 
+    @ApiModelProperty(name = "tableName", value = "表英文名")
     private String tableName;
+
+    @ApiModelProperty(name = "tableComment", value = "表中文名")
     private String tableComment;
+
+    @ApiModelProperty(name = "id", value = "表的guid")
     private String tableGuid;
+
+    @ApiModelProperty(name = "id", value = "数据源类型")
     private String DbType;
+
+    @ApiModelProperty(name = "id", value = "数据库")
     private String Db;
+
+    @ApiModelProperty(name = "id", value = "创建时间")
     private Date createTime;
+
 }
 
